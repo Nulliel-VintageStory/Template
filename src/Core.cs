ca801a84c80b93837a7837056baea93fe50f17f7
Nulliel-VintageStory <Nulliel-VintageStory@users.noreply.github.com>
2023-06-21 03:04:01 -0400

    initial commit

---
+++ src/Core.cs
@@ -0,0 +1,18 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+[assembly: ModInfo(
+	"__Template__",
+	Description = "",
+	Website     = "https://github.com/Nulliel-VintageStory/__Template__",
+	Authors     = new []{ "nulliel" }
+)]
+namespace __Template__;
+
+public class Core : ModSystem
+{
+	public override void Start(ICoreAPI api)
+	{
+		base.Start(api);
+	}
+}
